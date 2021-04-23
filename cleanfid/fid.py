8ccb6c328918670b38fa667ca29a88f2d7948dee
gaparmar <gparmar@ucsd.edu>
2021-04-23 08:28:17 -0400

    refactor the bool flgas

--- cleanfid/fid.py
+++ cleanfid/fid.py
@@ -74,35 +74,33 @@ Compute the inception features for a list of files
 """
 def get_files_features(l_files, model=None, num_workers=12,
                        batch_size=128, device=torch.device("cuda"),
-                       use_legacy_pytorch=False,
-                       use_legacy_tensorflow=False,
-                       custom_fn_resize=None, 
+                       mode="clean", custom_fn_resize=None, 
                        description=""):
     # define the model if it is not specified
     if model is None:
-        if use_legacy_pytorch:
+        if mode=="legacy_pytorch":
             model = build_feature_extractor(name="pytorch_inception")
         else:
             model = build_feature_extractor(name="torchscript_inception")
+    
     # build resizing function based on options
     if custom_fn_resize is not None:
         fn_resize = custom_fn_resize
-    elif use_legacy_pytorch:
+    elif mode=="legacy_pytorch":
         fn_resize = make_resizer("PyTorch", False, "bilinear", (299, 299))
-    elif use_legacy_tensorflow:
+    elif mode=="legacy_tensorflow":
         fn_resize = make_resizer("TensorFlow", False, "bilinear", (299, 299))
     else:
         fn_resize = make_resizer("PIL", False, "bicubic", (299, 299))
     # wrap the images in a dataloader for parallelizing the resize operation
     dataset = ResizeDataset(l_files, fn_resize=fn_resize)
     dataloader = torch.utils.data.DataLoader(dataset,
-                                             batch_size=batch_size, shuffle=False,
-                                             drop_last=False, num_workers=num_workers)
+                    batch_size=batch_size, shuffle=False,
+                    drop_last=False, num_workers=num_workers)
 
     # collect all inception features
     l_feats = []
-    pbar = tqdm(dataloader, desc=description)
-    for batch in pbar:
+    for batch in tqdm(dataloader, desc=description):
         l_feats.append(get_batch_features(batch, model, device))
     np_feats = np.concatenate(l_feats)
     return np_feats
@@ -113,8 +111,7 @@ Compute the inception features for a folder of features
 """
 def get_folder_features(fdir, model=None, num_workers=12,
                         num=None, batch_size=128, device=torch.device("cuda"),
-                        use_legacy_pytorch=False, use_legacy_tensorflow=False,
-                        custom_fn_resize=None, description=""):
+                        mode="clean", custom_fn_resize=None, description=""):
     # get all relevant files in the dataset
     files = sorted([file for ext in EXTENSIONS
                     for file in glob(os.path.join(fdir, f"*.{ext}"))])
@@ -122,8 +119,7 @@ def get_folder_features(fdir, model=None, num_workers=12,
         files = files[:num]
     np_feats = get_files_features(files, model, num_workers=num_workers,
                                   batch_size=batch_size, device=device,
-                                  use_legacy_pytorch=use_legacy_pytorch,
-                                  use_legacy_tensorflow=use_legacy_tensorflow,
+                                  mode=mode,
                                   custom_fn_resize=custom_fn_resize,
                                   description=description)
     return np_feats
@@ -141,26 +137,21 @@ Computes the FID score for a folder of images for a specific dataset
 and a specific resolution
 """
 def fid_folder(fdir, dataset_name, dataset_res,
-               model=None, use_legacy_pytorch=False,
-               use_legacy_tensorflow=False, num_workers=12,
+               model=None, mode="clean", num_workers=12,
                batch_size=128, device=torch.device("cuda")):
     # define the model if it is not specified
     if model is None:
-        if use_legacy_pytorch:
+        if mode=="legacy_pytorch":
             model = build_feature_extractor(name="pytorch_inception")
         else:
             model = build_feature_extractor(name="torchscript_inception")
     # Load reference FID statistics (download if needed)
-    ref_mu, ref_sigma = get_reference_statistics(dataset_name, dataset_res,
-                                                 use_legacy_tf=use_legacy_tensorflow,
-                                                 use_legacy_pyt=use_legacy_pytorch, seed=0)
+    ref_mu, ref_sigma = get_reference_statistics(dataset_name, dataset_res, mode=mode, seed=0)
     fbname = os.path.basename(fdir)
     # get all inception features for folder images
     np_feats = get_folder_features(fdir, model, num_workers=num_workers,
                                    batch_size=batch_size, device=device,
-                                   use_legacy_pytorch=use_legacy_pytorch,
-                                   use_legacy_tensorflow=use_legacy_tensorflow,
-                                   description=f"FID {fbname} : ")
+                                   mode=mode, description=f"FID {fbname} : ")
     mu = np.mean(np_feats, axis=0)
     sigma = np.cov(np_feats, rowvar=False)
     fid = frechet_distance(mu, sigma, ref_mu, ref_sigma)
@@ -172,27 +163,28 @@ and a specific resolution
 """
 def fid_model(G, dataset_name, dataset_res,
               model=None, z_dim=512, num_fid=50_000,
-              use_legacy_pytorch=False, use_legacy_tensorflow=False,
+              mode="clean",
               num_workers=0, batch_size=128,
               device=torch.device("cuda")):
     # define the model if it is not specified
     if model is None:
-        if use_legacy_pytorch:
+        if mode=="legacy_pytorch":
             model = build_feature_extractor(name="pytorch_inception")
         else:
             model = build_feature_extractor(name="torchscript_inception")
 
     # Load reference FID statistics (download if needed)
     ref_mu, ref_sigma = get_reference_statistics(dataset_name, dataset_res,
-                                                 use_legacy_tf=use_legacy_tensorflow,
-                                                 use_legacy_pyt=use_legacy_pytorch, seed=0)
+                                                 mode=mode, seed=0)
     # build resizing function based on options
-    if use_legacy_pytorch:
+    if mode=="legacy_pytorch":
         fn_resize = make_resizer("PyTorch", False, "bilinear", (299, 299))
-    elif use_legacy_tensorflow:
+    elif mode=="legacy_tensorflow":
         fn_resize = make_resizer("TensorFlow", False, "bilinear", (299, 299))
-    else:
+    elif mode=="clean":
         fn_resize = make_resizer("PIL", False, "bicubic", (299, 299))
+    else:
+        raise ValueError(f"spefied mode {mode} is not supported")
 
     # Generate test features
     num_iters = int(np.ceil(num_fid // batch_size))
@@ -211,8 +203,7 @@ def fid_model(G, dataset_name, dataset_res,
             resized_batch = torch.zeros(batch_size, 3, 299, 299)
             for idx in range(batch_size):
                 curr_img = img_batch[idx]
-                img_np = curr_img.cpu().numpy().transpose((1, 2, 0))
-                #img_pil = torchvision.transforms.ToPILImage()(curr_img)
+                img_np = curr_img.cpu().numpy().transpose((1, 2, 0)))
                 img_resize = fn_resize(img_np)
                 resized_batch[idx] = torch.tensor(img_resize.transpose((2, 0, 1)))
             feat = get_batch_features(resized_batch, model, device)
@@ -229,10 +220,8 @@ Computes the FID score between the two given folders
 """
 def compare_folders(fdir1, fdir2, num_workers=0,
                     batch_size=8, device=torch.device("cuda"),
-                    use_legacy_pytorch=False,
-                    use_legacy_tensorflow=False,
-                    ):
-    if use_legacy_pytorch:
+                    mode="clean"):
+    if mode == "legacy_pytorch":
         model = build_feature_extractor(name="pytorch_inception")
     else:
         model = build_feature_extractor(name="torchscript_inception")
@@ -240,8 +229,8 @@ def compare_folders(fdir1, fdir2, num_workers=0,
     # get all inception features for the first folder
     fbname1 = os.path.basename(fdir1)
     np_feats1 = get_folder_features(fdir1, model, num_workers=num_workers,
-                                    batch_size=batch_size, device=device, use_legacy_pytorch=use_legacy_pytorch,
-                                    use_legacy_tensorflow=use_legacy_tensorflow, description=f"FID {fbname1} : ")
+                                    batch_size=batch_size, device=device, mode=mode, 
+                                    description=f"FID {fbname1} : ")
     mu1 = np.mean(np_feats1, axis=0)
     sigma1 = np.cov(np_feats1, rowvar=False)
 
@@ -249,10 +238,13 @@ def compare_folders(fdir1, fdir2, num_workers=0,
     # get all inception features for the second folder
     fbname2 = os.path.basename(fdir2)
     np_feats2 = get_folder_features(fdir2, model, num_workers=num_workers,
-                                    batch_size=batch_size, device=device, use_legacy_pytorch=use_legacy_pytorch,
-                                    use_legacy_tensorflow=use_legacy_tensorflow, description=f"FID {fbname2} : ")
+                                    batch_size=batch_size, device=device, mode=mode,
+                                    description=f"FID {fbname2} : ")
 
     mu2 = np.mean(np_feats2, axis=0)
     sigma2 = np.cov(np_feats2, rowvar=False)
     fid = frechet_distance(mu1, sigma1, mu2, sigma2)
     return fid
+
+
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='clean-fid',
-    version='0.1.1',
+    version='0.1.1b',
     author="Gaurav Parmar",
     author_email="gparmar@andrew.cmu.edu",
     description="standardise the FID",
