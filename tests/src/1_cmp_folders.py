4f4cb18ec135abe6ad737f2abefcbcd77c8ef0b5
gaparmar <gparmar@ucsd.edu>
2021-12-24 16:47:25 -0500

    testing on cpu

--- tests/src/1_cmp_folders.py
+++ tests/src/1_cmp_folders.py
@@ -4,19 +4,19 @@ folder_fake = "tmp/folder_fake"
 folder_real = "tmp/folder_real"
 
 # check the clean FID
-score = fid.compute_fid(folder_real, folder_fake, mode="clean", batch_size=8, num_workers=20)
+score = fid.compute_fid(folder_real, folder_fake, mode="clean", batch_size=8, num_workers=20, device=torch.device("cpu")
 print(f"\nclean-fid score: {score:.4f}")
 if abs(score-75.1679)>1e-3:
     raise ValueError(f"clean FID does not match the expected value")
 
 # check the legacy pytorch fid
-score = fid.compute_fid(folder_real, folder_fake, mode="legacy_pytorch", batch_size=8, num_workers=20)
+score = fid.compute_fid(folder_real, folder_fake, mode="legacy_pytorch", batch_size=8, num_workers=20, device=torch.device("cpu")
 print(f"\nlegacy-pytorch-fid score: {score:.4f}")
 if abs(score-77.2019)>1e-3:
     raise ValueError(f"legacy pytorch FID does not match the expected value")
 
 # check the legacy tensorflow fid
-score = fid.compute_fid(folder_real, folder_fake, mode="legacy_tensorflow", batch_size=8, num_workers=20)
+score = fid.compute_fid(folder_real, folder_fake, mode="legacy_tensorflow", batch_size=8, num_workers=20, device=torch.device("cpu"))
 print(f"\nlegacy-tensorflow-fid score: {score:.4f}")
 if abs(score-76.9118)>1e-3:
     raise ValueError(f"legacy tensorflow FID does not match the expected value")
