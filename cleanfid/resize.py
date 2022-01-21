3b577224718f21022e40c9e188b1a0d589ca6c3b
gaparmar <gparmar@ucsd.edu>
2022-01-21 14:09:44 -0500

    fixed cv2 imports

--- cleanfid/resize.py
+++ cleanfid/resize.py
@@ -6,25 +6,6 @@ import numpy as np
 import torch
 from PIL import Image
 import torch.nn.functional as F
-import cv2
-
-
-dict_name_to_filter = {
-    "PIL": {
-        "bicubic": Image.BICUBIC,
-        "bilinear": Image.BILINEAR,
-        "nearest": Image.NEAREST,
-        "lanczos": Image.LANCZOS,
-        "box": Image.BOX
-    },
-    "OpenCV": {
-        "bilinear": cv2.INTER_LINEAR,
-        "bicubic": cv2.INTER_CUBIC,
-        "lanczos": cv2.INTER_LANCZOS4,
-        "nearest": cv2.INTER_NEAREST,
-        "area": cv2.INTER_AREA
-    }
-}
 
 
 def build_resizer(mode):
@@ -45,17 +26,31 @@ flags passed in.
 """
 def make_resizer(library, quantize_after, filter, output_size):
     if library == "PIL" and quantize_after:
+        name_to_filter = {
+            "bicubic": Image.BICUBIC,
+            "bilinear": Image.BILINEAR,
+            "nearest": Image.NEAREST,
+            "lanczos": Image.LANCZOS,
+            "box": Image.BOX
+        }
         def func(x):
             x = Image.fromarray(x)
-            x = x.resize(output_size, resample=dict_name_to_filter[library][filter])
+            x = x.resize(output_size, resample=name_to_filter[filter])
             x = x.clip(0, 255)
             x = np.asarray(x).astype(np.uint8)
             return x
     elif library == "PIL" and not quantize_after:
+        name_to_filter = {
+            "bicubic": Image.BICUBIC,
+            "bilinear": Image.BILINEAR,
+            "nearest": Image.NEAREST,
+            "lanczos": Image.LANCZOS,
+            "box": Image.BOX
+        }
         s1, s2 = output_size
         def resize_single_channel(x_np):
             img = Image.fromarray(x_np.astype(np.float32), mode='F')
-            img = img.resize(output_size, resample=dict_name_to_filter[library][filter])
+            img = img.resize(output_size, resample=name_to_filter[filter])
             return np.asarray(img).reshape(s1, s2, 1)
         def func(x):
             x = [resize_single_channel(x[:, :, idx]) for idx in range(3)]
@@ -86,7 +81,13 @@ def make_resizer(library, quantize_after, filter, output_size):
             return x
     elif library == "OpenCV":
         import cv2
-        name_to_filter = dict_name_to_filter["OpenCV"]
+        name_to_filter = {
+            "bilinear": cv2.INTER_LINEAR,
+            "bicubic": cv2.INTER_CUBIC,
+            "lanczos": cv2.INTER_LANCZOS4,
+            "nearest": cv2.INTER_NEAREST,
+            "area": cv2.INTER_AREA
+        }
         def func(x):
             x = cv2.resize(x, output_size, interpolation=name_to_filter[filter])
             x = x.clip(0, 255)
