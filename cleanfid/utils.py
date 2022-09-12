3bb787bb967e9cf4308e64003cb57290c3e9c599
gaparmar <gparmar@ucsd.edu>
2022-09-12 12:40:24 -0400

    supporting uppercase file extensions

--- cleanfid/utils.py
+++ cleanfid/utils.py
@@ -52,4 +52,4 @@ class ResizeDataset(torch.utils.data.Dataset):
 
 
 EXTENSIONS = {'bmp', 'jpg', 'jpeg', 'pgm', 'png', 'ppm',
-              'tif', 'tiff', 'webp', 'npy', 'JPEG'}
+              'tif', 'tiff', 'webp', 'npy', 'JPEG', 'JPG', 'PNG'}
